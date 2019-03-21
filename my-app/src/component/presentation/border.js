9ee409cb981f1fdab450ade6ec25d962573b7c3e
cc <397938635@qq.com>
2019-03-21 09:40:14 +0800

    init

--- my-app/src/component/presentation/border.js
+++ my-app/src/component/presentation/border.js
@@ -3,7 +3,7 @@ import './border.css'
 class Border extends Component {
     render() {
         return (
-            <svg className={'border-svg'}  xmlns="http://www.w3.org/2000/svg"   width="95%" height="95%" viewBox="0 0 400 400"  >
+            <svg className={'border-svg'}  xmlns="http://www.w3.org/2000/svg"   width="95%" height="95%" viewBox="0 0 400 400"  preserveAspectRatio="none slice">
                 <path d="M 0,0 400,0 400,400 0,400 0,0"  strokeWidth="1" stroke="cyan" fill="none"></path>
                 <path d="M 0,50  0,0 50,0" strokeWidth="10"  fill="none" stroke="cyan"></path>
                 <path d="M 0,350 0,400 50,400" strokeWidth="10"  fill="none" stroke="cyan"></path>
