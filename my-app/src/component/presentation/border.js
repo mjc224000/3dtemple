2134f4672521a3b705169fe3a06c927729996bbd
cc <397938635@qq.com>
2019-03-21 10:23:06 +0800

    init

--- my-app/src/component/presentation/border.js
+++ my-app/src/component/presentation/border.js
@@ -5,7 +5,7 @@ class Border extends Component {
         return (
             <svg className={'border-svg'}  xmlns="http://www.w3.org/2000/svg"   width="95%" height="95%" viewBox="0 0 400 400"  preserveAspectRatio="none slice">
                 <path d="M 0,0 400,0 400,400 0,400 0,0"  strokeWidth="1" stroke="cyan" fill="none"></path>
-                <path d="M 0,50  0,0 50,0" strokeWidth="10"  fill="none" stroke="cyan"></path>
+                <path d="M 0,30  0,0 25,0" strokeWidth="10"  fill="none" stroke="cyan"></path>
                 <path d="M 0,350 0,400 50,400" strokeWidth="10"  fill="none" stroke="cyan"></path>
                 <path d="M 350,400 400,400 400,350" strokeWidth="10"  fill="none" stroke="cyan"></path>
                 <path d="M 350,400 400,400 400,350" strokeWidth="10"  fill="none" stroke="cyan"></path>
